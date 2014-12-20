fa26c150ad7daef3b70c0bc074622a0e0396cad6
Daniel López Pérez <dlopezp@gmx.es>
2014-12-20 17:16:02 +0100

    Add PilotoBase class

---
+++ src/PilotoBase.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HOME3
+ */
+public class PilotoBase {
+    
+    private String nombre;
+    private String apellidos;
+    private Integer edad;
+    private Double altura;
+    private Double peso;
+    private Integer puntos;
+    private Double reflejos;
+    private Double agresividad;
+    private Double paciencia;
+    private Double valentia;
+    
+}
