f53dbfcdcfffe0435c07d019f6c7485a0cbdc2ff
Daniel López Pérez <dlopezp@gmx.es>
2014-12-27 16:37:31 +0100

    close #3

---
+++ src/formula/Coche.java
@@ -0,0 +1,102 @@
+package formula;
+
+public class Coche {
+ 
+    private String modelo;
+    private Double potencia;
+    private Double aerodinamica;
+    private Double neumaticos;
+    
+    public static final Double VALOR_MINIMO_CARACTERISTICAS = 0.0;
+    public static final Double VALOR_MAXIMO_CARACTERISTICAS = 5.0;
+    public static final Integer FACTOR_POTENCIA_RECTA = 20;
+    public static final Integer FACTOR_AERODINAMICA_RECTA = 30;
+    public static final Integer FACTOR_POTENCIA_CURVA = 10;
+    public static final Integer FACTOR_AERODINAMICA_CURVA = 10;
+    public static final Integer FACTOR_NEUMATICOS_CURVA = 10;
+    public static final Double INCREMENTO_MEJORA = 0.1;
+
+    Coche(String modelo, Double potencia, Double aerodinamica, Double neumaticos) {
+        this.modelo = modelo;
+        this.potencia = getValorCaracteristicaEnLimite(potencia);
+        this.aerodinamica = getValorCaracteristicaEnLimite(aerodinamica);
+        this.neumaticos = getValorCaracteristicaEnLimite(neumaticos);
+    }
+    
+    private Double getValorCaracteristicaEnLimite(Double valor) {
+        Double valorSeguro;
+        if (valor < 0) {
+            valorSeguro = 0.0;
+        } else if (valor > VALOR_MAXIMO_CARACTERISTICAS) {
+            valorSeguro = VALOR_MAXIMO_CARACTERISTICAS;
+        } else {
+            valorSeguro = valor;
+        }
+        return valorSeguro;
+    }
+    
+    public Double getVelocidadEnRecta() {
+        Double velocidad;
+        velocidad = potencia * FACTOR_POTENCIA_RECTA;
+        velocidad += (aerodinamica + FACTOR_AERODINAMICA_RECTA);
+        return velocidad;
+    }
+    
+    public Double getVelocidadEnCurva() {
+        Double velocidad;
+        velocidad = potencia * FACTOR_POTENCIA_CURVA;
+        velocidad += (aerodinamica + FACTOR_AERODINAMICA_CURVA);
+        velocidad += (neumaticos + FACTOR_NEUMATICOS_CURVA);
+        return velocidad;
+    }
+    
+    public void mejorar() {
+        setPotencia(potencia + INCREMENTO_MEJORA);
+        setAerodinamica(aerodinamica + INCREMENTO_MEJORA);
+        setNeumaticos(neumaticos + INCREMENTO_MEJORA);
+    }
+    
+    public Double getTiempoPorVuelta(Circuito circuito) {
+        Double tiempo = 0.0;
+        Double distanciaRecta = circuito.getDistanciaRecta();
+        Double velocidadRecta = getVelocidadEnRecta();
+        tiempo += velocidadRecta / distanciaRecta;
+        Double distanciaCurva = circuito.getDistanciaCurva();
+        Double velocidadCurva = getVelocidadEnCurva();
+        tiempo += velocidadCurva / distanciaCurva;
+        tiempo *= 3600;
+        return tiempo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(Double potencia) {
+        this.potencia = getValorCaracteristicaEnLimite(potencia);
+    }
+
+    public Double getAerodinamica() {
+        return aerodinamica;
+    }
+
+    public void setAerodinamica(Double aerodinamica) {
+        this.aerodinamica = getValorCaracteristicaEnLimite(aerodinamica);
+    }
+
+    public Double getNeumaticos() {
+        return neumaticos;
+    }
+
+    public void setNeumaticos(Double neumaticos) {
+        this.neumaticos = getValorCaracteristicaEnLimite(neumaticos);
+    }
+}
---
+++ test/formula/CocheTest.java
@@ -0,0 +1,84 @@
+package formula;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CocheTest {
+    
+    private String modelo = "FW37";
+    private Double potencia = 4.5;
+    private Double aerodinamica = 3.8;
+    private Double neumaticos = 4.0;
+    private Coche coche = new Coche(modelo, potencia, aerodinamica, neumaticos);
+    
+    private String nombre = "Spa-Francorchamps";
+    private Integer aforo = 1;
+    private Integer canon = 1;
+    private Double[] rectas = {1334.33, 1334.33, 1334.33};
+    private Double[] curvas = {600.0, 600.0, 600.0, 600.0, 600.0};
+    private Circuito circuito = new Circuito(nombre, aforo, canon, rectas, curvas);
+    
+    @Test
+    public void test_getValorCaracteristicaEnLimite() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Double valorMinimo = Coche.VALOR_MINIMO_CARACTERISTICAS;
+        Double valorMaximo = Coche.VALOR_MAXIMO_CARACTERISTICAS;
+        Class clase = coche.getClass();
+        Method metodo = null;
+        Class[] argumentos = new Class[1];
+        argumentos[0] = Double.class;
+        try {
+            metodo = clase.getDeclaredMethod("getValorCaracteristicaEnLimite", argumentos);
+        } catch (NoSuchMethodException e) {
+            fail("No existe método getValorCaracteristicaEnLimite");
+        }
+        metodo.setAccessible(true);
+        Object valorNegativo[] = {-1.0};
+        assertThat(valorMinimo, is(metodo.invoke(coche, valorNegativo)));
+        Object valorSuperior[] = {5.1};
+        assertThat(valorMaximo, is(metodo.invoke(coche, valorSuperior)));
+    }
+    
+    @Test
+    public void test_Mejorar() {
+        coche.mejorar();
+        Double potenciaEsperada = potencia + Coche.INCREMENTO_MEJORA;
+        assertThat(potenciaEsperada, is(coche.getPotencia()));
+        Double aerodinamicaEsperada = aerodinamica + Coche.INCREMENTO_MEJORA;
+        assertThat(aerodinamicaEsperada, is(coche.getAerodinamica()));
+        Double neumaticosEsperados = neumaticos + Coche.INCREMENTO_MEJORA;
+        assertThat(neumaticosEsperados, is(coche.getNeumaticos()));
+    }
+    
+    @Test
+    public void test_Get_Velocidad_En_Recta() {
+        Double velocidadEstimada;
+        velocidadEstimada = potencia * Coche.FACTOR_POTENCIA_RECTA;
+        velocidadEstimada += (aerodinamica + Coche.FACTOR_AERODINAMICA_RECTA);
+        assertThat(velocidadEstimada, is(coche.getVelocidadEnRecta()));
+    }
+    
+    @Test
+    public void test_Get_Velocidad_En_Curva() {
+        Double velocidadEstimada;
+        velocidadEstimada = potencia * Coche.FACTOR_POTENCIA_CURVA;
+        velocidadEstimada += (aerodinamica + Coche.FACTOR_AERODINAMICA_CURVA);
+        velocidadEstimada += (neumaticos + Coche.FACTOR_NEUMATICOS_CURVA);
+        assertThat(velocidadEstimada, is(coche.getVelocidadEnCurva()));
+    }
+    
+    @Test
+    public void test_Get_Tiempo_Por_Vuelta() {
+        Double tiempoEstimado = 0.0;
+        Double distanciaRecta = circuito.getDistanciaRecta();
+        Double velocidadRecta = coche.getVelocidadEnRecta();
+        tiempoEstimado += velocidadRecta / distanciaRecta;
+        Double distanciaCurva = circuito.getDistanciaCurva();
+        Double velocidadCurva = coche.getVelocidadEnCurva();
+        tiempoEstimado += velocidadCurva / distanciaCurva;
+        tiempoEstimado *= 3600;
+        assertThat(tiempoEstimado, is(coche.getTiempoPorVuelta(circuito)));
+    }
+}
