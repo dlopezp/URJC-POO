b0cefe06a806ed875919c991470c487a7128eab7
edugithub <bf_eduardo@yahoo.es>
2014-12-31 13:17:04 +0100

    EscuderiaAdapter creado

---
+++ src/gui/EscuderiaAdapter.java
@@ -0,0 +1,41 @@
+package gui;
+
+
+import formula.Escuderia;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class EscuderiaAdapter {
+    private static EscuderiaAdapter instance = null;
+    private static FormulaAdapter<Escuderia> adapter = null;
+    private static final String FICHERO = "ficheros" + System.getProperty("file.separator") + "Escuderia.dat";
+    
+    private EscuderiaAdapter() {}
+    
+    private FormulaAdapter<Escuderia> getAdapter() {
+       if (adapter == null) {
+           adapter = new FormulaAdapter<>();
+       } 
+       return adapter;
+    }
+    
+    public static EscuderiaAdapter getInstance() {
+        if (instance == null) {
+            instance = new EscuderiaAdapter();
+        }
+        return instance;
+    }
+    
+    public void guardar(ArrayList<Escuderia> escuderias) throws IOException {
+        getAdapter().save(escuderias, FICHERO);
+    }
+    
+    public ArrayList<Escuderia> leer() throws ClassNotFoundException {
+        return getAdapter().read(FICHERO);
+    }
+    
+    
+}
+
+
