930996aab1d75382fb09f9b515ca3bbf953a6a88
edugithub <bf_eduardo@yahoo.es>
2015-01-04 10:56:55 +0100

    Escuderia.java Modifico método compareTo

--- src/formula/Escuderia.java
+++ src/formula/Escuderia.java
@@ -191,8 +191,17 @@ public class Escuderia implements Comparable<Escuderia>, Serializable {
 
     @Override
     public int compareTo(Escuderia e){
-        return e.getPuntos() - getPuntos();
+        int comp = 0;
+        if (getPuntos() < e.getPuntos()){
+            comp = -1;
+        }
+        else if (getPuntos() > e.getPuntos()){
+            comp = 1;
+        }
+        return comp;
     }
+    
+    
     
     @Override
     public String toString() {
